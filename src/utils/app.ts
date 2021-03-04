e98cc1acec69f0dcfb434de16d5d892454be3c21
Takeshi Iwana <iwatakeshi@gmail.com>
2021-03-04 14:42:35 -0700

    Add app.ts

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,2 +1,13 @@
+import { readFileSync } from 'fs';
+import { PackageJson } from '../types';
 import AppPath from './app-path';
+import * as JSONC from 'comment-json';
+export default class App {
+  static get package(): PackageJson {
+    try {
       return JSONC.parse(readFileSync(AppPath.package, 'utf-8'));
+    } catch {
+      return { name: '' };
+    }
+  }
+}
